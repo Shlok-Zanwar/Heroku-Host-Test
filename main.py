7ebbfd41e665e2bb0afc5b801dbac39f6227ed11
Shlok-Zanwar <shlokzanwar14@gmail.com>
2022-04-03 14:15:54 +0530

    Ready APP

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from Routers import model_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"]
+)
+
+
+app.include_router(model_router.router)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
